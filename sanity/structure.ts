bbbfae3159a023db29c22c8914b39c674cf7f46e
Benzida18 <benzida18@icloud.com>
2025-11-13 00:17:54 +0000

    Remove unused Sanity structure file

--- sanity/structure.ts
+++
@@ -1,9 +0,0 @@
-import { StructureResolver } from "sanity/structure";
-
-export const structure: StructureResolver = (S) =>
-    S.list()
-        .title("Content")
-        .items([
-            // Insight Articles
-            S.documentTypeListItem("insightArticle").title("Insight Articles"),
-        ]);
